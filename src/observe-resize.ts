19235cc88bc8d552097c8ef482b66999eea28c69
Sean Wong <seanwong24@live.com>
2021-12-15 02:36:01 -0600

    add options for observeResize

--- src/observe-resize.ts
+++ src/observe-resize.ts
@@ -1,11 +1,12 @@
 export function observeResize(
   element: HTMLElement,
-  handlers: ((entry: ResizeObserverEntry) => void)[]
+  handlers: ((entry: ResizeObserverEntry) => void)[],
+  options?: ResizeObserverOptions
 ) {
   const resizeObserver = new ResizeObserver((entries) => {
     entries.forEach((entry) => {
       handlers.forEach((handler) => handler.call(this, entry));
     });
   });
-  resizeObserver.observe(element);
+  resizeObserver.observe(element, options);
 }
